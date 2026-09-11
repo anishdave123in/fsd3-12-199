bda0e793ff31cc8c9c84bdbee43a28d5b2ca4ad5
ANISH DAVE <anishdave123in@gmail.com>
2026-09-11 15:03:44 +0530

    commited

--- LAB 3/prg4.js
+++ LAB 3/prg4.js
@@ -1,20 +1,31 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200,{"consent-type":"application/json"});
-//Writehead se batate hai client ko hi tumare pass kya ane wala hai
-//200 means ok
-//201 means created
-//404 not found
-//500
+  res.writeHead(200, { "content-type": "application/json" });
+  console.log("Client URL:", req.url);
+  // Writehead se batate hai client ko ki tumhare pass kya aane wala hai
 
-const products={
-    name:"Iphone" ,
-    price: 100000,
-    qty: 1
-    discount: 10
-
-};
-res.end(JSON.stringify(products));
+  const product = [
+    {
+      name: "Iphone",
+      price: 85000,
+      qty: 2,
+      discount: 15,
+    },
+    {
+      name: "macbook",
+      price: 85000,
+      qty: 2,
+      discount: 30,
+    },
+    {
+      name: "tablet",
+      price: 16000,
+      qty: 1,
+      discount: 20,
+    },
+  ];
+  res.end(JSON.stringify(product));
 });
-server.listen(3000,()=>console.log
+
+server.listen(3000, () => console.log("prg4 is running at 3000..."));
